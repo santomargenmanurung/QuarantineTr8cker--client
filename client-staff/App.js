e9986378ed4332c0e9fd03fa4123308a78a87b14
GustantoC <GustantoC@gmail.com>
2021-12-12 20:41:20 +0700

    Fix minor mistakes

--- client-staff/App.js
+++ client-staff/App.js
@@ -1,7 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Provider } from 'react-redux'
-import store from './store'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Provider } from "react-redux"
+import store from "./store"
 import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
